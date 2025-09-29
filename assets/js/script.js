2ba279d04437395399dab0598f354c3e57dc034d
Edis Vrtagić <101829021+EdisVrtagic@users.noreply.github.com>
2025-09-29 07:11:49 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('navMenu');
-    hamburger.addEventListener('click', () => {
+const navMenu = document.getElementById('navMenu');
+const navLinks = document.querySelectorAll('.nav a');
+
+hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
 });
